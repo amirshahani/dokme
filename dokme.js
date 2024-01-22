05b58a9caa02ae34cd98a1549f5fe8ee5a595541
amir shahani <amir74shahani@gmail.com>
2024-01-22 20:52:41 +0330

    commir two

--- dokme.js
+++ dokme.js
@@ -1,3 +1,10 @@
 const btn=document.querySelector('.btn');
 
- 
+window.addEventListener('scroll' , ()=>{
+    if(window.pageYOffset>300){
+        btn.classList.add('active');
+    }
+    else{
+        btn.classList.remove('active');
+    }
+})  
